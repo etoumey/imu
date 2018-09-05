8d47d0cb11b04af9b1573fad1f66c7a69e2aeb55
etoumey <etoumey@gmail.com>
2018-09-05 00:57:33 +0000

    Should probably work with accelerometer toooo

--- main.py
+++ main.py
@@ -47,12 +47,16 @@ def calibrate(sensor): #Implement Kalman Filter to quickly calibrate sensor
 	print (xGyrEst, yGyrEst, zGyrEst)
 	return(xAccEst, xGyrEst, yGyrEst, zGyrEst)	
 
+
+
+
+# Main
 sensor = mpu6050(0x68)
 [accOff, xGyrOff, yGyrOff, zGyrOff] = calibrate(sensor)
 
 
 print "Begin rotation"
-n = 5000 # Number of Samples
+n = 10000 # Number of Samples
 xAngle = 0
 yAngle = 0
 zAngle = 0
@@ -60,13 +64,38 @@ lastTime = time.time()
 initialTime = lastTime
 outFile = open("outFile.dat", "w")
 
+#Some more Kalman initializations
+xAccEst = 1.0
+yAccEst = 1.0
+zAccEst = 1.0
+xGyrEst = 0.0
+yGyrEst = 0.0
+zGyrEst = 0.0
+accR = .05
+gyrR = .05
+xAccP = .5
+yAccP = .5
+zAccP = .5
+xGyrP = .5
+yGyrP = .5
+zGyrP = .5
+
+
+
+
 for i in range(0,n):
 	dataGyro = sensor.get_gyro_data()
 	currentTime = time.time()
 	dt = currentTime - lastTime
-	xAngle += dt*(dataGyro['x'] - xGyrOff)
-	yAngle += dt*(dataGyro['y'] - yGyrOff)
-	zAngle += dt*(dataGyro['z'] - zGyrOff)
+#	[xGyrEst, xGyrP] = kalman(xGyrEst, dataGyro['x'] - xGyrOff, xGyrP,gyrR)
+#        [yGyrEst, yGyrP] = kalman(yGyrEst, dataGyro['y'] - yGyrOff, yGyrP,gyrR) 
+#        [zGyrEst, zGyrP] = kalman(zGyrEst, dataGyro['z'] - zGyrOff, zGyrP,gyrR) 
+	xGyrEst = dataGyro['x'] - xGyrOff
+	yGyrEst = dataGyro['y'] - yGyrOff
+	zGyrEst = dataGyro['z'] - zGyrOff
+	xAngle += dt*(xGyrEst)
+	yAngle += dt*(yGyrEst)
+	zAngle += dt*(zGyrEst)
 	lastTime = currentTime
 	outFile.write("%f %f %f %f\n" % (currentTime - initialTime,xAngle, yAngle, zAngle))
 outFile.close()
